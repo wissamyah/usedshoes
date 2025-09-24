3058e4efed65f10ffcf2654519a7261239257962
Wissam Yahfoufi <wissam.yahfoufi@gmail.com>
2025-09-24 11:43:55 +0100

    Add dist folder for deployment

---
+++ dist/assets/archive-BnS4defz.js
@@ -0,0 +1,6 @@
+import{c as e}from"./index-CnsNA1Vq.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["rect",{width:"20",height:"5",x:"2",y:"3",rx:"1",key:"1wp1u1"}],["path",{d:"M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8",key:"1s80jp"}],["path",{d:"M10 12h4",key:"a56b0p"}]],a=e("archive",c);export{a as A};
