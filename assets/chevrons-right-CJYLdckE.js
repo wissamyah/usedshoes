cc62305775649633c20934f157efee071a85388c
Wissam Yahfoufi <wissam.yahfoufi@gmail.com>
2025-10-01 20:06:52 +0100

    Updates

---
+++ assets/chevrons-right-CJYLdckE.js
@@ -0,0 +1,16 @@
+import{c as e}from"./index-DwJYWGdQ.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],h=e("chevron-left",o);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"m11 17-5-5 5-5",key:"13zhaf"}],["path",{d:"m18 17-5-5 5-5",key:"h8a8et"}]],s=e("chevrons-left",t);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"m6 17 5-5-5-5",key:"xnjwq"}],["path",{d:"m13 17 5-5-5-5",key:"17xmmf"}]],a=e("chevrons-right",n);export{s as C,h as a,a as b};
