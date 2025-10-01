08ac01ee255636747457a0be58c6312be2145a0d
Wissam Yahfoufi <wissam.yahfoufi@gmail.com>
2025-10-01 20:12:39 +0100

    Updates

---
+++ assets/funnel-DpEE9LkM.js
@@ -0,0 +1,11 @@
+import{c as o}from"./index-DzAdPBXS.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],t=o("chevron-right",n);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M10 20a1 1 0 0 0 .553.895l2 1A1 1 0 0 0 14 21v-7a2 2 0 0 1 .517-1.341L21.74 4.67A1 1 0 0 0 21 3H3a1 1 0 0 0-.742 1.67l7.225 7.989A2 2 0 0 1 10 14z",key:"sc7q7i"}]],a=o("funnel",c);export{t as C,a as F};
